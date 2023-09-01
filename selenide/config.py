92278403b2ee723024e96b303070bc1c3a3729d9
F4lanker <nikberezovskiy@gmail.com>
2023-09-01 15:05:10 +0700

    new_branch_first_comm

--- selenide/config.py
+++ selenide/config.py
@@ -1,11 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-Chrome_options = webdriver.ChromeOptions()
-driver = webdriver.Remote(
-    command_executor="http://localhost:4444/wd/hub",
-    options=Chrome_options
-    options.selonide.options.enableVNC = True
+# создание объекта опций
+chrome_options = Options()
+capabilities = {
+    "browserName": "chrome",
+    "selenoid:options": {
+        "enableVNC": True
+    }
+}
 
-)
-driver.get("http://www.google.com")
-driver.quit()
+# установка опции
+chrome_options.add_argument("start-maximized")
+capabilities.update(chrome_options.to_capabilities())
+
+
+# инициализация драйвера с использованием опций
+driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub', options=chrome_options)
+# Пример простого теста
+driver.get("https://www.google.com")
+print(driver.title)
