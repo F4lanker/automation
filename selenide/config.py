a4d25412b993d37387f500b7938205c18322c9fe
F4lanker <nikberezovskiy@gmail.com>
2023-09-01 16:26:41 +0700

    last update for Friday on the branch

--- selenide/config.py
+++ selenide/config.py
@@ -9,11 +9,9 @@ capabilities = {
         "enableVNC": True
     }
 }
-
 # установка опции
 chrome_options.add_argument("start-maximized")
 capabilities.update(chrome_options.to_capabilities())
-
 
 # инициализация драйвера с использованием опций
 driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub', options=chrome_options)
